cd491905839e1fb103695f04145baf01534907de
cuteandcudly81 <muhammed_yusuf@hotmail.com>
2022-03-30 10:55:32 +0200

    Initial commit

---
+++ src/main/java/se/lexikon/Arraysdemo.java
@@ -0,0 +1,31 @@
+package se.lexikon;
+
+public class Arraysdemo {
+
+    public static void main(String[] args) {
+        /*
+        String [] stringArray = new String[3];
+        int[] numbers = new int[3];
+                char[] words = new char[4];
+
+         int[] numbers2 = new int[] {2,6,3,9};
+         String[] StringArrays2 = new String[]{"Test1", "Test2", "Test3"};
+         String[] StringArrays3 = {"Test1", "Test2", "Test3"};
+         char[] words2 = {'J','A','V','A'};
+         */
+    }
+
+    public static void ex1(){
+        int [] numbers = new int[4];
+        numbers[0] = 11;
+        numbers[1] = 23;
+        numbers[2] = 39;
+        numbers[3] = 63;
+        System.out.println(numbers[0] + " " + numbers[1] + " " + numbers[2] + " " + numbers[3]);
+
+
+
+
+    }
+
+}
---
+++ src/main/java/se/lexikon/ExerciseOne.java
@@ -0,0 +1,37 @@
+package se.lexikon;
+import java.util.Arrays;
+
+public class ExerciseOne {
+
+    public static void main(String[] args) {
+
+
+        /*
+        int numbers[] = new int[]{11, 23, 39, 63};
+        for (int number : numbers){
+            System.out.print(number + " ");
+
+         */
+        /*
+        int numbers[] = new int[4];
+        numbers[0] = 11;
+        numbers[1] = 23;
+        numbers[2] = 39;
+        numbers[3] = 63;
+
+        for(int number : numbers){
+            System.out.print(number + " ");
+         */
+
+        
+
+        }
+
+
+        }
+
+
+
+
+
+
